6a25513c292eba5d3fd27c5010d505acee38a111
UMESHPC\Umesh Benglauru <umeshbs@gmail.com>
2021-08-05 13:28:03 +0530

    commited sql

---
+++ Account.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.Account    
+(    
+     
+  AccountNo numeric NOT NULL,    
+  AccountHolder character varying(500) NOT NULL,
+  AccountType character varying(100) NOT NULL,  
+  Amount numeric  
+      
+);
